98a1b8480cf98731b36f308dd363021f083d6ddf
Lobo <ezad821q@hotmail.com>
2023-10-30 08:32:50 -0300

    proyecto sin ventana Flotante funcional

---
+++ filtroPrepFab6/main.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const alertt = '#BD1F1F';
+const ok = '#55A317';
+
+const datosSector = (instalacionID,botonVentilador,dataVentilador,botonFiltroVent,dataFiltroVent,botonPicos,dataPicos,botonRpm,dataRpm,botonCarro,dataCarro,direccion) => {
+
+const sector = new XMLHttpRequest();
+    sector.onreadystatechange = () => {
+
+        if(sector.readyState == XMLHttpRequest.DONE){
+            if(sector.status == 200) {
+
+                const buttonVentilador = document.getElementById(botonVentilador);
+                const buttonFiltroVent = document.getElementById(botonFiltroVent);
+                const buttonPicos = document.getElementById(botonPicos);
+                const buttonRpm = document.getElementById(botonRpm);
+                const buttonCarro = document.getElementById(botonCarro);
+
+                console.log(sector.responseText);
+                const data = JSON.parse(sector.responseText);
+
+                const instalacion = data.instalacion;
+                const ventilador = parseInt(data.datos[0].presVentilador);
+                const limitVentilador = data.datos[0].limVentilador;
+                const filtroVentilador = parseInt(data.datos[0].filtroVentilador);
+                const limitFiltroVentilador = data.datos[0].limFiltroVentilador;
+                const picos = parseInt(data.datos[0].presPicos);
+                const limitPicos = data.datos[0].limPicos;
+                const rpmFiltro = data.datos[0].rpmFiltro;
+                const limitRpmFiltro = data.datos[0].limRpmFiltro;
+                const carro = data.datos[0].Carro;
+                const limitCarro = data.datos[0].limCarro;
+
+                const infoVentilador = `${ventilador} Pa`;
+                const infoFiltroVentilador = `${filtroVentilador} Pa`;
+                const infoPicos = `${picos} Pa`;
+                const infoRpm = `${rpmFiltro} RPM`;
+                const infoCarro = `${carro}`;
+
+                document.getElementById(instalacionID).textContent = instalacion;
+                document.getElementById(dataVentilador).textContent = infoVentilador;
+                document.getElementById(dataFiltroVent).textContent = infoFiltroVentilador;
+                document.getElementById(dataPicos).textContent = infoPicos;
+                document.getElementById(dataRpm).textContent = infoRpm;
+                document.getElementById(dataCarro).textContent = infoCarro;
+
+                (ventilador < limitVentilador) ? buttonVentilador.style.fill = alertt : buttonVentilador.style.fill = ok;
+
+                (filtroVentilador > limitFiltroVentilador) ? buttonFiltroVent.style.fill = alertt : buttonFiltroVent.style.fill = ok;
+
+                (picos < limitPicos) ? buttonPicos.style.fill = alertt : buttonPicos.style.fill = ok;
+
+                (rpmFiltro < limitRpmFiltro) ? buttonRpm.style.fill = alertt : buttonRpm.style.fill = ok;
+
+                (carro < limitCarro) ? buttonCarro.style.fill = alertt : buttonCarro.style.fill = ok;
+
+            } else console.log('error',sector);
+        }
+
+    }
+
+    sector.open("GET",direccion, true);
+    sector.send();
+
+}
+
+
+const actualizarDatos = () =>{
+    const fab = datosSector('instalacionText','ventiladorData','ventiladorDataText','diferencialData','diferencialDataText','picosData','picosDataText','rpmData','rpmFiltroDataText','carroData','carroDataText',"http://192.168.3.122:1880/dataFab6bobFiltros");
+
+}
+
+actualizarDatos();
+
+setInterval(actualizarDatos,60000);
--- main.js
+++ main.js
@@ -1,29 +1,70 @@
 "use-strict"
 
-const alert = '#BD1F1F';
-const ok = '#2bc12cff';
+const alertClima = '#BD1F1F';
+const okClima = '#2bc12cff';
 
-const datosSector = (botonFiltro,botonClima,textTemp,textHum,direccion) => {
+const neutral = '#8F8F8F';
+
+const alertFiltro  = '#DC5252';
+const okFiltro = '#24A024';
+
+const offline = '#DE7511';
+
+const mouseOver = '#58B7D3';
+
+const mouseOut = '#333';
+
+const fab6ClimaPrep = document.getElementById('filtroPrepFab6');
+
+const ventanaFlotante = document.getElementById('ventanaFlotante');
+
+
+
+fab6ClimaPrep.addEventListener("mouseover", ()=>{
+    fab6ClimaPrep.style.fill = mouseOver;
+    let x = event.clientX + 10; // Agregar un desplazamiento a la derecha
+    let y = event.clientY + 8; // Agregar un desplazamiento hacia abajo
+    ventanaFlotante.style.left = x + "px";
+    ventanaFlotante.style.top = y + "px";
+    ventanaFlotante.style.display = "block";
+});
+
+
+fab6ClimaPrep.addEventListener("mouseout", ()=>{
+    fab6ClimaPrep.style.fill = mouseOut;
+    ventanaFlotante.style.display = "none";
+});
+
+fab6ClimaPrep.addEventListener("click", ()=>{
+    window.location.href = 'filtroPrepFab6/index.html'; // Cambia esta URL a la que desees redirigir
+});
+
+const puestoClimaRef = (botonTemp,botonHum,textTemp,textHum,direccion) => {
     const sector = new XMLHttpRequest();
     sector.onreadystatechange = () => {
-        const buttonFiltro = document.getElementById(botonFiltro);
-        const buttonClima = document.getElementById(botonClima);
+        const buttonTemp = document.getElementById(botonTemp);
+        const buttonHum = document.getElementById(botonHum);
 
         if (sector.readyState == XMLHttpRequest.DONE){
             if(sector.status == 200) {
-                console.log(sector.responseText);
-                const datos = JSON.parse(sector.responseText);
-                const estadoFiltro = datos.estado;
-                const estadoClima = datos.estadoClima;
-                const infoTemp = `T: ${parseInt(datos.temperatura)}°C`;
-                const infoHum = `H: ${parseInt(datos.humedad)}%`;
+                const date = JSON.parse(sector.responseText);
+                const temp = parseInt(date.datos[0].tempFabrica);
+                const minTemper = date.datos[0].minTemp;
+                const maxTemper = date.datos[0].maxTemp;
+                const minHum = date.datos[0].minHum;
+                const maxHum = date.datos[0].maxHum; 
+                const hum = parseInt(date.datos[0].humedad);
+                const infoTemp = `T: ${temp}°C`;
+                const infoHum = `H: ${hum}%`;
                 
                 document.getElementById(textTemp).textContent = infoTemp;
                 document.getElementById(textHum).textContent = infoHum;
 
-                (estadoFiltro === 1) ? buttonFiltro.style.fill = ok : buttonFiltro.style.fill = alert;
+    
+                (temp < minTemper || temp > maxTemper ) ? buttonTemp.style.fill = alertClima : buttonTemp.style.fill = okClima;
 
-                (estadoClima === 1) ? buttonClima.style.fill = ok : buttonClima.style.fill = alert;
+                (hum < minHum || hum > maxHum) ? buttonHum.style.fill = alertClima : buttonHum.style.fill = okClima;
+
             } else console.log('error',sector);
         }
     }
@@ -31,6 +72,35 @@ const datosSector = (botonFiltro,botonClima,textTemp,textHum,direccion) => {
     sector.open("GET", direccion, true);
     sector.send();
 
+}
+
+const puestoClima = (botonTemp,botonHum,textTemp,textHum,direccion) =>{
+    const sector = new XMLHttpRequest();
+    sector.onreadystatechange = () => {
+        const buttonTemp = document.getElementById(botonTemp);
+        const buttonHum = document.getElementById(botonHum);
+
+        if (sector.readyState == XMLHttpRequest.DONE){
+            if(sector.status == 200) {
+                console.log(sector.responseText);
+                const date = JSON.parse(sector.responseText);
+                const temp = parseInt(date.datos[0].tempFabrica);
+                const hum = parseInt(date.datos[0].humedad);
+                const infoTemp = `T: ${temp}°C`;
+                const infoHum = `H: ${hum}%`;
+                
+                document.getElementById(textTemp).textContent = infoTemp;
+                document.getElementById(textHum).textContent = infoHum;
+
+    
+                 buttonTemp.style.fill = neutral; buttonHum.style.fill = neutral;
+
+            } else console.log('error',sector);
+        }
+    }
+
+    sector.open("GET", direccion, true);
+    sector.send();
 }
 
 const botonF = (botonFiltro,direccion) => {
@@ -43,7 +113,7 @@ const botonF = (botonFiltro,direccion) => {
                 const datos = JSON.parse(sector.responseText);
                 const estadoFiltro = datos.estado;
 
-                (estadoFiltro === 1) ? buttonFiltro.style.fill = ok : buttonFiltro.style.fill = alert;
+                (estadoFiltro === 1) ? buttonFiltro.style.stroke = okFiltro : buttonFiltro.style.stroke = alertFiltro;
             } else console.log('error',sector);
         }
     }
@@ -52,50 +122,74 @@ const botonF = (botonFiltro,direccion) => {
     sector.send();
 }
 
+
+
 const actualizarDatos = () => {
-    const fab6Prep = datosSector('filtroPrepFab6','climaPrepFab6','tempPrepFab6','humPrepFab6',"http://192.168.3.122:1880/dataFab6prepClima");
 
-    const fab6Cont = datosSector('filtroContFab6','climaContFab6','tempContFab6','humContFab6',"http://192.168.3.122:1880/datafab6contClima");
+    const fab6PrepClima = puestoClimaRef ('climaPrepTFab6','climaPrepHFab6','tempPrepFab6','humPrepFab6',"http://192.168.3.122:1880/dataFab6prepClima");
+    const fab6PrepFiltro = botonF('filtroPrepFab6',"http://192.168.3.122:1880/dataFab6prepFiltro");
 
-    const fab6Bob = datosSector('filtroBobFab6','climaBobFab6','tempBobFab6','humBobFab6',"http://192.168.3.122:1880/datafab6bobClima");
+    const fab6ContClima = puestoClimaRef ('climaContTFab6','climaContHFab6','tempContFab6','humContFab6',"http://192.168.3.122:1880/dataFab6contClima");
+    const fab6ContFiltro = botonF('filtroContFab6',"http://192.168.3.122:1880/dataFab6contFiltros");
 
-    const fab4prep = datosSector('filtroClimaFab4','climaPrepFab4','tempPrepFab4','humPrepFab4',"http://192.168.3.122:1880/dataFab4prepClima");
+    const fab6BobinajeClima = puestoClimaRef ('climaBobTFab6','climaBobHFab6','tempBobFab6','humBobFab6',"http://192.168.3.122:1880/dataFab6bobClima");
+    const fab6BobinajeFiltro = botonF ('filtroBobFab6',"http://192.168.3.122:1880/dataFab6bobFiltros");
 
-    const fab4openEnd = datosSector('filtroClimaFab4','climaOpen-EndFab4','tempOpenEndFab4','humOpenEndFab4',"http://192.168.3.122:1880/dataFab4openClima");
+    const fab4PreparacionClima = puestoClimaRef('climaPrepTFab4','climaPrepHFab4','tempPrepFab4','humPrepFab4',"http://192.168.3.122:1880/dataFab4prepClima");
+    const fab4ClimaFiltro = botonF('filtroClimaFab4',"http://192.168.3.122:1880/dataFab4climaFiltro");
+    const fab4OpenEndClima = puestoClima('climaOpen-EndTFab4','climaOpen-EndHFab4','tempOpenEndFab4','humOpenEndFab4',"http://192.168.3.122:1880/dataFab4openClima");
 
-    const fab4Retorcido = datosSector('filtroRetorFab4','climaRetorFab4','tempRetorFab4','humRetorFab4',"http://192.168.3.122:1880/dataFab4retorcidoClima");
+    const fab4RetorFiltro = botonF('filtroRetorFab4',"http://192.168.3.122:1880/dataFab4retorcidoFiltro");
+    const fab4RetorcidoClima = puestoClima('climaRetorTFab4','climaRetorHFab4','tempRetorFab4','humRetorFab4',"http://192.168.3.122:1880/dataFab4retorcidoClima");
 
-    const fab9Preparacion = datosSector('filtroPrepFab9','climaPrepFab9-3','tempPrepFab9','humPrepFab9',"http://192.168.3.122:1880/dataFab9prepClima");
+    const fab9Carda1 = puestoClimaRef('climaPrepTFab9-1','climaPrepHFab9-1','tempCardaFab9','humCardaFab9',"http://192.168.3.122:1880/dataFab9carda-1Clima");
+    const fab9Carda2 = puestoClima('climaPrepTFab9-2','climaPrepHFab9-2','tempCardas-2Fab9','humCardas-2Fab9',"http://192.168.3.122:1880/dataFab9carda-2Clima");
+    const fab9PrepFiltro = botonF('filtroPrepFab9',"http://192.168.3.122:1880/dataFab9prepFiltro");
+    const fab9PrepClima = puestoClima('climaPrepTFab9-3','climaPrepHFab9-3','tempPrepFab9','humPrepFab9',"http://192.168.3.122:1880/dataFab9prepClima");
 
-    const fab9Carda1 = datosSector('filtroPrepFab9','climaPrepFab9-1','tempCardaFab9','humCardaFab9',"http://192.168.3.122:1880/dataFab9carda-1Clima");
+    const fab9OpenEndClima = puestoClimaRef('climaOpenEndTFab9','climaOpenEndHFab9','tempOpenEndFab9','humOpenEndFab9',"http://192.168.3.122:1880/dataFab9openEndClima");
+    const fab9OpenFiltro = botonF('filtroOpenEndFab9',"http://192.168.3.122:1880/dataFab9openEndFiltro");
 
-    const fab9carda2 = datosSector('filtroPrepFab9','climaPrepFab9-2','tempCardas-2Fab9','humCardas-2Fab9',"http://192.168.3.122:1880/dataFab9carda-2Clima");
+    const fab3Ex8Filtro = botonF('filtroEX-8Fab3',"http://192.168.3.122:1880/dataFab3ex8");
+    const fab3C80Clima = puestoClima('climaC80TFab3','climaC80HFab3','tempC80Fab3','humC80Fab3',"http://192.168.3.122:1880/dataFab3C80Clima");
 
-    const fab9OpenEnd = datosSector('filtroOpenEndFab9','climaOpenEndFab9','tempOpenEndFab9','humOpenEndFab9',"http://192.168.3.122:1880/dataFab9openEnd");
+    const fab3PeinadorasClima = puestoClimaRef('climaPeiTFab3','climaPeiHFab3','tempPeiFab3','humPeiFab3',"http://192.168.3.122:1880/dataFab3PeinadorasClima");
+    const fab3PeinadorasFiltro = botonF('filtroPrepFab3-5',"http://192.168.3.122:1880/dataFab3PeinadorasFiltro");
 
-    const fab3C80 = datosSector('filtroEX-8Fab3','climaC80Fab3','tempC80Fab3','humC80Fab3',"http://192.168.3.122:1880/dataFab3C80Clima");
+    const fab3ManuaresClima = puestoClimaRef('climaManTFab3','climaManHFab3','tempManFab3','humManFab3',"http://192.168.3.122:1880/dataFab3ManuaresClima");
+    const fab3PrepaFiltro = botonF('filtroPrepFab3',"http://192.168.3.122:1880/dataFab3PrepFiltro");
 
-    const fab3Peinadoras = datosSector('filtroPrepFab3','climaPeiFab3','tempPeiFab3','humPeiFab3',"http://192.168.3.122:1880/dataFab3PeinadorasClima");
+    const fab3CardaFiltro = botonF('filtroCardasFab3',"http://192.168.3.122:1880/dataFab3CardaFiltro");
 
-    const fab3Manuares = datosSector('filtroCardasFab3','climaManFab3','tempManFab3','humManFab3',"http://192.168.3.122:1880/dataFab3ManuaresClima");
+    const fab3G30Clima = puestoClimaRef('climaG30TFab3','climaG30HFab3','tempG30Fab3','humG30Fab3',"http://192.168.3.122:1880/dataFab3G30Clima");
+    const fab3G30Filtro = botonF('filtroContFab3',"http://192.168.3.122:1880/dataFab3G30Filtro");
 
-    const fab3PeinadorasF = botonF('filtroPrepFab3-5',"http://192.168.3.122:1880/dataFab3PeinadorasFClima");
+    const fab3G33Clima = puestoClimaRef('climaG33TFab3','climaG33HFab3','tempG33Fab3','humG33Fab3',"http://192.168.3.122:1880/dataFab3G33Clima");
+    const fab3G33Filtro = botonF('filtroG33Fab3',"http://192.168.3.122:1880/dataFab3G33Filtro");
 
-    const fab3G30 = datosSector('filtroContFab3','climaG30Fab3','tempG30Fab3','humG30Fab3',"http://192.168.3.122:1880/dataFab3ContinuasClima");
+    const fab3BobClima = puestoClimaRef('climaBobinajeTFab3','climaBobinajeHFab3','tempBobinajeFab3','humBobinajeFab3',"http://192.168.3.122:1880/dataFab3BobinajeClima");
+    const fab3BobFiltro = botonF('filtroBobFab3',"http://192.168.3.122:1880/dataFab3BobinajeFiltro");
 
-    const fab3G33 = datosSector('filtroG33Fab3','climaG33Fab3','tempG33Fab3','humG33Fab3',"http://192.168.3.122:1880/dataFab3G33Clima");
+    const fab1ColorClima = puestoClimaRef('climaColorTFab1','climaColorHFab1','tempColorFab1','humColorFab1',"http://192.168.3.122:1880/dataFab1ColorClima");
+    const fab1ColorFiltro = botonF('filtroColorFab1',"http://192.168.3.122:1880/dataFab1ColorFiltro");
 
-    const fab3Bobinaje = datosSector('filtroBobFab3','climaBobinajeFab3','tempBobinajeFab3','humBobinajeFab3',"http://192.168.3.122:1880/dataFab3BobinajeClima");
+    const fab1PeinadorasClima = puestoClimaRef('climaPeinadorasTFab1','climaPeinadorasHFab1','tempPeiFab1','humPeiFab1',"http://192.168.3.122:1880/dataFab1PeinadorasClima");
+    const fab1ManuaresClima = puestoClimaRef('climaManuaresTFab1','climaManuaresHFab1','tempManuaresFab1','humManuaresFab1',"http://192.168.3.122:1880/dataFab1ManuaresClima");
+    const fab1PrepFiltro = botonF('filtroPrepFab1',"http://192.168.3.122:1880/dataFab1PrepFiltro");
+    const fab1CardasFiltro = botonF('filtroCardasFab1',"http://192.168.3.122:1880/dataFab1CardaFiltros");
 
-    const fab1Color = datosSector('filtroColorFab1','climaColorFab1','tempColorFab1','humColorFab1',"http://192.168.3.122:1880/dataFab1ColorClima");
+    const fab1ContFiltro = botonF('filtroContinuasFab1',"http://192.168.3.122:1880/dataFab1ContinuasFiltro");
+    const fab1Continuas1Clima = puestoClima('climaContinuasTFab1-1','climaContinuasHFab1-1','tempContFab1-1','humContFab1-1',"http://192.168.3.122:1880/dataFab1ContinuasClima");
+    const fab1Continuas2Clima = puestoClimaRef('climaContinuasTFab1-2','climaContinuasHFab1-2','tempContFab1-2','humContFab1-2',"http://192.168.3.122:1880/dataFab1Continuas2Clima");
 
-    const fab1Peinadoras = datosSector('filtroPrepFab1','climaPeinadorasFab1','tempPeiFab1','humPeiFab1',"http://192.168.3.122:1880/dataFab1PeinadorasClima");
-    
-    const fab1Continuas1 = datosSector('filtroContinuasFab1','climaContinuasFab1-1','tempContFab1-1','humContFab1-1',"http://192.168.3.122:1880/dataFab1ContinuasClima");
-
-    const fab1Continuas2 = datosSector('filtroContinuasFab1','climaContinuasFab1-2','tempContFab1-2','humContFab1-2',"http://192.168.3.122:1880/dataFab1Continuas2Clima");
-
-    const fab1Vortex = datosSector('filtroContinuasFab1','climaVortexFab1','tempVortexFab1','humVortexFab1',"http://192.168.3.122:1880/dataFab1VortexClima");
+    const fab1EmpaqueClima = puestoClimaRef('climaEmpaqueTFab1','climaEmpaqueHFab1','tempEmpaqueFab1','humEmpaqueFab1',"http://192.168.3.122:1880/dataFab1EmpaqueClima");
+    const fab1VortexClima = puestoClima('climaVortexTFab1','climaVortexHFab1','tempVortexFab1','humVortexFab1',"http://192.168.3.122:1880/dataFab1VortexClima");
+    const fab1Vortex1Clima = puestoClima('climaVortexTFab1-1','climaVortexHFab1-1','tempVortexFab1-1','humVortexFab1-1',"http://192.168.3.122:1880/dataFab1Vortex-1Clima");
+    const fab1Vortex2Clima = puestoClima('climaVortexTFab1-2','climaVortexHFab1-2','tempVortexFab1-2','humVortexFab1-2',"http://192.168.3.122:1880/dataFab1Vortex-2Clima");
+    const fab1Vortex3Clima = puestoClima('climaVortexTFab1-3','climaVortexHFab1-3','tempVortexFab1-3','humVortexFab1-3',"http://192.168.3.122:1880/dataFab1Vortex-3Clima");
+    const fab1BobinajeClima = puestoClimaRef('climaBobinajeTFab1','climaBobinajeHFab1','tempBobinajeFab1','humBobinajeFab1',"http://192.168.3.122:1880/dataFab1BobinajeClima");
+    const fab1Batan = botonF('filtroBatanFab1',"http://192.168.3.122:1880/dataFab1BatanFiltro");
+    const fab1Cotonia = botonF('filtroCotoniaFab1',"http://192.168.3.122:1880/dataFab1Cotonia");
 }
 
 actualizarDatos();
